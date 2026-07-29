c6a8dc9ac724f3fc65811da354680cc909c5b508
Template Janitor <template.janitor@example.com>
2026-07-29 09:07:32 +0000

    Cleanup template and initialize repository

--- cmake/Linker.cmake
+++ cmake/Linker.cmake
@@ -1,20 +1,20 @@
-macro(myproject_configure_linker project_name)
-  set(myproject_USER_LINKER_OPTION
+macro(sysarchPalyground_configure_linker project_name)
+  set(sysarchPalyground_USER_LINKER_OPTION
     "DEFAULT"
       CACHE STRING "Linker to be used")
-    set(myproject_USER_LINKER_OPTION_VALUES "DEFAULT" "SYSTEM" "LLD" "GOLD" "BFD" "MOLD" "SOLD" "APPLE_CLASSIC" "MSVC")
-  set_property(CACHE myproject_USER_LINKER_OPTION PROPERTY STRINGS ${myproject_USER_LINKER_OPTION_VALUES})
+    set(sysarchPalyground_USER_LINKER_OPTION_VALUES "DEFAULT" "SYSTEM" "LLD" "GOLD" "BFD" "MOLD" "SOLD" "APPLE_CLASSIC" "MSVC")
+  set_property(CACHE sysarchPalyground_USER_LINKER_OPTION PROPERTY STRINGS ${sysarchPalyground_USER_LINKER_OPTION_VALUES})
   list(
     FIND
-    myproject_USER_LINKER_OPTION_VALUES
-    ${myproject_USER_LINKER_OPTION}
-    myproject_USER_LINKER_OPTION_INDEX)
+    sysarchPalyground_USER_LINKER_OPTION_VALUES
+    ${sysarchPalyground_USER_LINKER_OPTION}
+    sysarchPalyground_USER_LINKER_OPTION_INDEX)
 
-  if(${myproject_USER_LINKER_OPTION_INDEX} EQUAL -1)
+  if(${sysarchPalyground_USER_LINKER_OPTION_INDEX} EQUAL -1)
     message(
       STATUS
-        "Using custom linker: '${myproject_USER_LINKER_OPTION}', explicitly supported entries are ${myproject_USER_LINKER_OPTION_VALUES}")
+        "Using custom linker: '${sysarchPalyground_USER_LINKER_OPTION}', explicitly supported entries are ${sysarchPalyground_USER_LINKER_OPTION_VALUES}")
   endif()
 
-  set_target_properties(${project_name} PROPERTIES LINKER_TYPE "${myproject_USER_LINKER_OPTION}")
+  set_target_properties(${project_name} PROPERTIES LINKER_TYPE "${sysarchPalyground_USER_LINKER_OPTION}")
 endmacro()
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <sysarchPalyground/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
